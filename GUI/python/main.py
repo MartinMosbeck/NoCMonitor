330633ef178ac8824f279f0777ec7c46dddcd34c
MartinMosbeck <martin.mosbeck@gmx.at>
2015-12-01 12:45:12 +0100

    main GUI development - SavePoint 2
    Signed-off-by: MartinMosbeck <martin.mosbeck@gmx.at>

--- GUI/python/main.py
+++ GUI/python/main.py
@@ -7,6 +7,7 @@ import sys
 import math
 import string
 from enum import Enum
+from BitVector import *
 
 class MyTemplate(string.Template):
     delimiter = "%%"
@@ -42,7 +43,9 @@ class NoC_Configurator:
         self.connect_directory = ""
         self.flit_data_width = 1
         self.flit_buffer_depth = 1
-        self.default_traffic = Traffic_Patterns.random
+        self.default_traffic_pattern = Traffic_Patterns.random
+        self.default_packet_injection_rate = 0.1
+        self.default_flits_per_packet = 1
 
     def new_config(self):
         self.configNoC = QtWidgets.QDialog()
@@ -58,7 +61,9 @@ class NoC_Configurator:
         self.connect_directory = configNoC_ui.connect_directory.text()
         self.flit_data_width = configNoC_ui.flit_data_width.value()
         self.flit_buffer_depth = configNoC_ui.flit_buffer_depth.value()
-        self.default_traffic = Traffic_Patterns(configNoC_ui.default_traffic.currentIndex())
+        self.default_traffic_pattern = Traffic_Patterns(configNoC_ui.default_traffic_pattern.currentIndex())
+        self.default_packet_injection_rate = configNoC_ui.default_packet_injection_rate.value()
+        self.default_flits_per_packet = configNoC_ui.default_flits_per_packet.value()
 
     def configure_PEs(self):
         if(self.mesh_configured == False):
@@ -83,14 +88,16 @@ class NoC_Configurator:
 
     def setPEDefaults(self):
         #TODO: handle random and manulal
-        self.PE_pattern = []
+        self.PE_traffic_pattern = []
         self.PE_dest = []
-        self.PE_duty = []
+        self.PE_packet_injection_rate = []
+        self.PE_flits_per_packet = []
 
         for i in range (0,self.mesh_width*self.mesh_height):
-            self.PE_pattern.append(self.default_traffic)
-            self.PE_dest.append(self.pattern_to_dest(self.default_traffic))
-            self.PE_duty.append(50)                        
+            self.PE_traffic_pattern.append(self.default_traffic_pattern)
+            self.PE_dest.append(self.pattern_to_dest(self.default_traffic_pattern,i))
+            self.PE_packet_injection_rate.append(self.default_packet_injection_rate)
+            self.PE_flits_per_packet.append(self.default_flits_per_packet)                        
 
     def configurePE(self,X):
         self.configPE = QtWidgets.QDialog()
@@ -102,84 +109,50 @@ class NoC_Configurator:
 
     def setValues_PEui(self,ui,X):
         #TODO: handle random and manulal
-        ui.traffic_pattern.setCurrentIndex(self.PE_pattern[X].value)
+        ui.PE_label.setText("PE_{0} Parameters".format(X))
+        ui.traffic_pattern.setCurrentIndex(self.PE_traffic_pattern[X].value)
         ui.implied_destination.setText(str(self.PE_dest[X]))
-        ui.duty_cycle.setValue(self.PE_duty[X])
+        ui.packet_injection_rate.setValue(self.PE_packet_injection_rate[X])
+        ui.flits_per_packet.setValue(self.PE_flits_per_packet[X])
 
     def PE_config_accepted(self,ui,X):
         #TODO: handle random and manulal
-        self.PE_pattern[X] = Traffic_Patterns(ui.traffic_pattern.currentIndex())
-        self.PE_dest[X] = self.pattern_to_dest(self.PE_pattern[X])
-        self.PE_duty[X] = ui.duty_cycle.value()
+        self.PE_traffic_pattern[X] = Traffic_Patterns(ui.traffic_pattern.currentIndex())
+        self.PE_dest[X] = self.pattern_to_dest(self.PE_traffic_pattern[X],X)
+        self.PE_packet_injection_rate[X] = ui.packet_injection_rate.value()
+        self.PE_flits_per_packet[X] = ui.flits_per_packet.value()
 
-    def pattern_to_dest(self,pattern):
-        return 1
+    def pattern_to_dest(self,pattern,X):
+        # return -1 is reserved for random
+        bit_len = math.ceil(math.log(self.mesh_width*self.mesh_height,2))
+        bv = BitVector(intVal=X,size=bit_len)
+
+        if(pattern==Traffic_Patterns.random):
+            return -1;
+        
+        elif(pattern==Traffic_Patterns.bit_complement):
+            return int(~bv)
+
+        elif(pattern==Traffic_Patterns.bit_reverse):
+            return int(bv.reverse())
+
+        elif(pattern==Traffic_Patterns.bit_rotation):
+            return int(bv<<1)
+
+        elif(pattern==Traffic_Patterns.transpose):
+           return 1
+
+        elif(pattern==Traffic_Patterns.shuffle):
+            t = bv[bit_len-1]
+            bv[bit_len-1] = bv[0]
+            bv[0] = t
+            return int(bv)
+
+        elif(pattern==Traffic_Patterns.manual):
+            return self.PE_dest[X]
 
     def create_Files(self):
-        #create PE_X files
-        filename_tmp="../../templates/PE_X.temp"
-        for i in range(0,self.mesh_width*self.mesh_height):
-            parameters={"X":str(i),"DEST":str(self.PE_dest[i]),"DUTY_PERIOD":str(math.ceil(100/self.PE_duty[i]))}
-         
-            with open (filename_tmp, "r") as template_file:
-                data=template_file.read()
-
-            filename_out =self.connect_directory+"/"+"PE_{0}.v".format(i)
-            with open (filename_out, "w") as output_file:
-                s = MyTemplate(data)
-                output_file.write(s.substitute(parameters)) 
-
-        '''
-        #create connecting wires
-        connections = ""
-        filename_tmp="../../templates/connection.temp"
-        for i in range(0,self.mesh_width*self.mesh_height):
-            parameters={"X":str(i)}
-         
-            with open (filename_tmp, "r") as template_file:
-                data=template_file.read()
-
-            s = MyTemplate(data)
-            s.substitute(parameters)
-            connections=connections+s
-
-        #create PE instantiations
-        create_PEs = ""
-        filename_tmp="../../templates/create_PE.temp"
-        for i in range(0,self.mesh_width*self.mesh_height):
-            parameters={"X":str(i)}
-         
-            with open (filename_tmp, "r") as template_file:
-                data=template_file.read()
-
-            s = MyTemplate(data)
-            output_file.write(s.substitute(parameters)) 
-            createPEs=createPEs+s
-
-        mappings_mkNetwork=""
-        filename_tmp="mapping_MkNetwork.temp"
-        for i in range(0,self.mesh_width*self.mesh_height):
-            parameters={"X":str(i)}
-         
-            with open (filename_tmp, "r") as template_file:
-                data=template_file.read()
-
-            s = MyTemplate(data)
-            output_file.write(s.substitute(parameters)) 
-            mappings_mkNetwork=mappings_mkNetwork+s
-
-        #create testbench
-        filename_tmp="../../templates/testbench.temp"
-        parameters={"CONNECTIONS":connections,"CREATE_PEs":createPEs,"MAPPINGS_mkNetwork":mappings_mkNetwork}
-         
-        with open (filename_tmp, "r") as template_file:
-            data=template_file.read()
-
-        filename_out =self.connect_directory+"/testbench.v"
-        with open (filename_out, "w") as output_file:
-            s = MyTemplate(data)
-            output_file.write(s.substitute(parameters))
-        '''
+        return 0
         
 
                 
